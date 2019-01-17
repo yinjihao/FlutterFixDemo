153541882fc53cbd05e6281cca97c56c58bd0a24
yinjihao <yinjihao@yiche.com>
2019-01-17 18:55:34 +0800

    fix flutter

--- my_flutter/lib/main.dart
+++ my_flutter/lib/main.dart
@@ -14,9 +14,54 @@ class MyApp extends StatelessWidget {
           title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+//控件
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new RandomWordState();
+  }
+}
+
+class RandomWordState extends State<RandomWords> {
+  //数组
+  final _suggestions = <WordPair>[];
+
+  //样式
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        //在每一列之前，添加一个1像素的分割线widget
+        if (i.isOdd) {
+          return new Divider();
+        }
+        //
+        // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+        // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+        final index = i ~/ 2;
+        // 如果是建议列表中最后一个单词对
+        if (index >= _suggestions.length) {
+          // ...接着再生成10个单词对，然后添加到建议列表
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var wordPair = new WordPair.random();
+    Text mTextView = new Text(wordPair.asPascalCase);
+    return mTextView;
   }
 }
